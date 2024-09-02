ed9f31a4eb12ca48ccb302d5a6cd54c82f80bd04
Evgeny Korostelev <ddc_smoke@mail.ru>
2024-09-02 10:05:20 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        MontyHallParadox simulation = new MontyHallParadox();
+        simulation.runSimulation();
+        simulation.printResults();
+    }
+}
